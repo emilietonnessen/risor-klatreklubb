c6355d51e96002d6ebe8ab978906a64e4950215e
Emizzari <emilie@tonnessen.com>
2021-02-08 15:02:50 +0100

    Removing carousel from iOS devices

--- js/about.js
+++ js/about.js
@@ -1,4 +1,4 @@
-import { menuIcon, menu, body } from "./components/abstracts/variables.js";
+import { menuIcon } from "./components/abstracts/variables.js";
 import mobileMenu from "./components/menu/mobileMenu.js";
 import displayCurrentYear from "./components/copyright/currentYear.js"
 import carousel from "./components/gallery/carousel.js";
@@ -8,30 +8,26 @@ menuIcon.addEventListener("click", mobileMenu);
 displayCurrentYear();
 
 
-// IOS
-const iOS = false;
+// IOS Variables
+let iOS = false;
 const appleDevices = ['iPad', 'iPhone', 'iPod'];
 const sectionCarousel = document.querySelector(".section-carousel");
 
+// Checks if the user is using iPad, iPhone or iPod
 if (appleDevices.indexOf(navigator.platform) > -1) {
     iOS = true;
 } 
 
-
-
-if (appleDevices === true) {
+// Remove Carousel if iOS = true
+if (iOS === true) {
 	console.log("iOS is true")
-	sectionCarousel.style.display = "none";
+	sectionCarousel.remove();
 } else {
-	console.log("iOS is false")
+	window.addEventListener('load', () => {
+		var carousels = document.querySelectorAll('.carousel');
+
+		for (var i = 0; i < carousels.length; i++) {
+			carousel(carousels[i]);
+		}
+	});
 }
-
-
-
-window.addEventListener('load', () => {
-	var carousels = document.querySelectorAll('.carousel');
-
-	for (var i = 0; i < carousels.length; i++) {
-		carousel(carousels[i]);
-	}
-});
